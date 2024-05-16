e34f2cfe1ca940cb3bcff2612d728c8364e28449
João Gouveia <joaotalonegouveia@tecnico.ulisboa.pt>
2024-05-16 11:34:43 +0100

    Move everything to the same file

--- js/main-script.js
+++ js/main-script.js
@@ -20,8 +20,148 @@ export { scene };
 /* GLOBAL VARIABLES */
 //////////////////////
 let scene, renderer;
-let currCamera;
+let currCamera,
+    lateralCamera,
+    topCamera,
+    frontalCamera,
+    broadPCamera,
+    broadOCamera;
+let pressedKeys = {
+    1: false,
+    2: false,
+    3: false,
+    4: false,
+    5: false,
+    6: false,
+    7: false,
+    q: false,
+    a: false,
+    w: false,
+    s: false,
+    e: false,
+    d: false,
+    r: false,
+    f: false,
+};
 
+////////////////////////////
+/*        EVENTS          */
+////////////////////////////
+function bindEvents() {
+    window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("keyup", onKeyUp);
+}
+
+///////////////////////
+/* KEY DOWN CALLBACK */
+///////////////////////
+function onKeyDown(e) {
+    "use strict";
+    if (!pressedKeys.hasOwnProperty(e.key)) {
+        return;
+    }
+
+    pressedKeys[e.key] = isFinite(e.key) ? !e.repeat : true;
+}
+
+///////////////////////
+/* KEY UP CALLBACK */
+///////////////////////
+function onKeyUp(e) {
+    "use strict";
+    if (!pressedKeys.hasOwnProperty(e.key)) {
+        return;
+    }
+
+    pressedKeys[e.key] = false;
+}
+
+//////////////////////
+/* CREATE CAMERA(S) */
+//////////////////////
+function createCameras() {
+    createLateralCamera();
+    createFrontalCamera();
+    createTopCamera();
+    createBroadPerpectiveCamera();
+    createBroadOrthographicCamera();
+}
+
+function createLateralCamera() {
+    "use strict";
+    let aspectRatio = window.innerWidth / window.innerHeight;
+    let viewSize = 70;
+    lateralCamera = new THREE.OrthographicCamera(
+        (aspectRatio * viewSize) / -2,
+        (aspectRatio * viewSize) / 2,
+        viewSize / 2,
+        viewSize / -2,
+        1,
+        1000,
+    );
+    lateralCamera.position.set(0, 30, 70);
+    lateralCamera.lookAt(0, 30, 0);
+}
+
+function createFrontalCamera() {
+    "use strict";
+    let aspectRatio = window.innerWidth / window.innerHeight;
+    let viewSize = 70;
+    frontalCamera = new THREE.OrthographicCamera(
+        (aspectRatio * viewSize) / -2,
+        (aspectRatio * viewSize) / 2,
+        viewSize / 2,
+        viewSize / -2,
+        1,
+        1000,
+    );
+    frontalCamera.position.set(100, 30, 0);
+    frontalCamera.lookAt(0, 30, 0);
+}
+
+function createTopCamera() {
+    "use strict";
+    let aspectRatio = window.innerWidth / window.innerHeight;
+    let viewSize = 70;
+    topCamera = new THREE.OrthographicCamera(
+        (aspectRatio * viewSize) / -2,
+        (aspectRatio * viewSize) / 2,
+        viewSize / 2,
+        viewSize / -2,
+        1,
+        1000,
+    );
+    topCamera.position.set(0, 70, 0);
+    topCamera.lookAt(0, 0, 0);
+}
+
+function createBroadPerpectiveCamera() {
+    "use strict";
+    broadPCamera = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000,
+    );
+    broadPCamera.position.set(40, 40, 40);
+    broadPCamera.lookAt(0, 25, 0);
+}
+
+function createBroadOrthographicCamera() {
+    "use strict";
+    let aspectRatio = window.innerWidth / window.innerHeight;
+    let viewSize = 70;
+    broadOCamera = new THREE.OrthographicCamera(
+        (aspectRatio * viewSize) / -2,
+        (aspectRatio * viewSize) / 2,
+        viewSize / 2,
+        viewSize / -2,
+        1,
+        1000,
+    );
+    broadOCamera.position.set(40, 40, 40);
+    broadOCamera.lookAt(0, 25, 0);
+}
 
 /////////////////////
 /* CREATE SCENE(S) */
